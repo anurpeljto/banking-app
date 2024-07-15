e45aee36a5c46c8b36eb3fe02ae65a322e9eb2ea
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-07-16 01:17:16 +0200

    Create structure and component boilerplates

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from './Navbar';
+import Billing from './Billing';
+import Business from './Business';
+import Button from './Button';
+import Hero from './Hero';
+import FeedbackCard from './FeedbackCard';
+import GetStarted from './GetStarted';
+import CTA from './CTA';
+import Footer from './Footer';
+import Stats from './Stats';
+import Clients from './Clients';
+import CardDeal from './CardDeal';
+import Testimonials from './Testimonials';
+
+export {
+    Navbar,
+    Billing,
+    Footer,
+    Business,
+    Button,
+    CardDeal,
+    FeedbackCard,
+    Testimonials,
+    Stats,
+    Clients,
+    CTA,
+    GetStarted,
+    Hero
+}
