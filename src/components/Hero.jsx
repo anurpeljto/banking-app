3eefa3cfd4ad64719e873e56d10856f7707abaf6
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-09-18 23:24:47 +0200

    Add react typed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,8 @@
 import React, {useRef, useEffect, useState} from 'react';
 import styles from '../style';
-import {discount, robot, robotarm, robotarmmod, reacticon} from '../assets';
+import {discount, robot, robotarm, robotarmmod, reacticon, logo} from '../assets';
 import GetStarted from './GetStarted';
+import { ReactTyped } from "react-typed";
 
 const Hero = () => {
   return (
@@ -9,7 +10,7 @@ const Hero = () => {
       
 
        <div className='flex flex-1 h-full justify-center items-center mt-5 mx-4 mb-[300px] flex-col'>
-          <h1 className={`${styles.heading2}`}>Welcome to <span className='text-gradient'>anurpeljto.live</span></h1>
+          <h1 className={`${styles.heading2}`}>Welcome to <ReactTyped className='text-gradient' strings={['anurpeljto.live']} loop typeSpeed={40} backSpeed={50}></ReactTyped></h1>
           <div className='flex flex-col justify-between items-center h-full'>
             <p className={`${styles.paragraph}`}>Please scroll or use the navigation to find out more about me</p>
           </div>
