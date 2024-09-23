0fe82b9acd74c5e38389d42eefbad631da05c94c
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-09-23 12:43:45 +0200

    Update warning

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,7 +6,7 @@ const Portfolio = () => {
   return (
     <section className='flex flex-col items-center justify-center gap-5'>
         <h1 className='text-6xl text-gradient font-bold self-center'>My Portfolio</h1>
-        <p className={`text-sm ${style.paragraph}`}><strong>NOTE:</strong> Many of these websites are deployed on render and other free hosting services. In case of lowered activity, these websites will slow down the servers. <br /> If the websites seem empty (no fetched data, like products), please refresh until they appear.</p>
+        <p className={`text-sm ${style.paragraph}`}><strong>NOTE:</strong> Many of these websites are deployed on render and other free hosting services. In case of lowered activity, these websites will slow down the servers. <br /> If the website seems empty (no fetched data, like products), please wait for the request to finish. In case it does not, please refresh and try again</p>
         <div className='grid grid-cols-1 sm:grid-cols-3 mx-auto sm:gap-24 gap-10 justify-between'>
             <PortfolioCard href='https://nashville-preds.vercel.app/' img='/nashville2.png' description='Nashville Predators Page (NextJS + Typescript)'/>
             <PortfolioCard href='https://scandiweb-test-1.onrender.com/' img='/scandi2.png' description='ReactJS + GraphQL (PHP) + MySQL  eCommerce site'/>
